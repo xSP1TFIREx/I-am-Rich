cc64c2de52f9d004f38207c9df9b62ee7a8d3e7d
Dmitry <107319523+xSP1TFIREx@users.noreply.github.com>
2022-09-14 15:43:11 +0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Дмитрий on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
